40ec631a4cd1828f3ef298f9cdfea964a6a3782b
Ludovic Estival <estivall@3il.fr>
2026-01-07 14:12:25 +0100

    Eliminate menu duplicates

--- contact.php
+++ contact.php
@@ -4,101 +4,71 @@ session_start();
 <!DOCTYPE html>
 
 <html lang="fr">
-	<head>
-		<title>TRUC</title>
 
-		<link rel="stylesheet" href="style.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<head>
+	<title>TRUC</title>
 
-	</head>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+		integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-	<body>
-<!--*************** MENU ***************-->
-<nav class="navbar">
-	<li class="toggle">
-		<ul class ="toggle-item"><i class="fa fa-bars menu" aria-hidden="true"> </i></ul>
-	</li>
-   <ul class="nav-links">
-      	<li class="nav-item"><a href="index.php">ACCUEIL</a></li>
-      	<li class="nav-item"><a href="produits.php">LES PRODUITS</a></li>
-	  	<li class="nav-item"><a href="video.php">VIDEO</a></li>
-		<li class="nav-item"><a href="contact.php">NOUS CONTACTER</a></li>
-<?php 
-	if (isset($_SESSION['id'])) {	
-		echo "<li class='nav-item'><a href='administration.php'>ADMINISTRATION</a></li>";
-	}
-	if(isset($_SESSION['id'])) {
-		echo "<li class='nav-item'><a href='deconnexion.php'>DECONNEXION</a></li>";
-	}else{
-		echo "<li class='nav-item'><a href='connexion.php'>CONNEXION</a></li>";
-	}
-?>
-    </ul>
+</head>
 
-	<img src="./images/scierie.gif" style="width:70px; margin:5px;">
-</nav>
+<body>
+	<?php include "menu.php" ?>
 
+	<div class="contactContainer">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script type="text/javascript">
+		<div class="rightContainer">
 
-	$(document).ready(function(){
+			<div class="email">
+				<h2> EMAIL </h2>
+				<p> scierie.gineste@wanadoo.fr
+				<p>
+			</div>
 
-		$('.menu').click(function(){
-			
-			$('ul').toggleClass('active');
-		})
-	})
+			<div class="telephone">
+				<h2> TELEPHONE </h2>
+				<p> +33 9 70 35 54 09
+				<p>
+			</div>
 
-</script> 
-<!--*************** END MENU ***************-->
+			<div class="adresse">
+				<h2> ADRESSE </h2>
+				<ul>
+					<li> Route de Rodez
+					<li>
+					<li> 12220
+					<li>
+					<li> MONTBAZENS
+					<li>
+				</ul>
+			</div>
 
-		<div class="contactContainer">
+			<div class="reseauxSociaux">
+				<h2> NOUS SUIVRE </h2>
 
-			<div class="rightContainer">
-
-				<div class = "email">
-					<h2> EMAIL </h2>
-					<p> scierie.gineste@wanadoo.fr <p>
-				</div>
-
-				<div class = "telephone">
-					<h2> TELEPHONE </h2>
-					<p> +33 9 70 35 54 09 <p>
-				</div>
-
-				<div class = "adresse">
-					<h2> ADRESSE </h2>
-					<ul>
-						<li> Route de Rodez <li>
-						<li> 12220 <li>
-						<li> MONTBAZENS <li>
-					</ul>
-				</div>
-
-				<div class = "reseauxSociaux">
-					<h2> NOUS SUIVRE </h2>
-
-					<ul class="logo">
-						<li class="facebook"><a href="https://www.facebook.com/Scierie-du-Fargal-613509152159633/" target="_blank"><img src="images/facebook.png"></a></li>
-					</ul>
-				</div>
-
+				<ul class="logo">
+					<li class="facebook"><a href="https://www.facebook.com/Scierie-du-Fargal-613509152159633/"
+							target="_blank"><img src="images/facebook.png"></a></li>
+				</ul>
 			</div>
 
 		</div>
 
-<!--*************** PIED DE PAGE ***************-->
-<footer id="footer">
-<ul class="footer-links">
-    <li class="footer-item">©Projet 3iL</li>
-    <li class="footer-item"><a href="#" target="_blank"><img id="logo" src="images/facebook.png"></a></li>
-    <li class="footer-item">Site test</li>
-<ul/>
-</footer>
-<!--*************** PIED DE PAGE ***************-->
+	</div>
 
-	</body>
+	<!--*************** PIED DE PAGE ***************-->
+	<footer id="footer">
+		<ul class="footer-links">
+			<li class="footer-item">©Projet 3iL</li>
+			<li class="footer-item"><a href="#" target="_blank"><img id="logo" src="images/facebook.png"></a></li>
+			<li class="footer-item">Site test</li>
+			<ul />
+	</footer>
+	<!--*************** PIED DE PAGE ***************-->
+
+</body>
 
 </html>
